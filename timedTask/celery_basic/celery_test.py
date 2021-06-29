645f7a855ce0b251f3e5da1e17b656243dbb5cee
SeafyLiang <seafyliang@icloud.com>
2021-06-29 10:50:09 +0800

    celery+flask 异步请求测试

--- timedTask/celery_basic/celery_test.py
+++ timedTask/celery_basic/celery_test.py
@@ -7,13 +7,25 @@
 2021/6/29 09:13   SeafyLiang   1.0          python使用celery
 """
 from task import add
+from flask import Flask
 
-if __name__ == '__main__':
+app = Flask(__name__)  # __name__ 表示对应程序名称
+
+
+@app.route("/celery_flask_test")
+def index():
     print("Start Task ...")
     result = add.delay(2, 8)
     print("result:", result)  # 存到redis之后，返回的id
     print("result_id:", result.id)  # 存到redis之后，返回的id
     print("result:", result.get())  # 方法返回值
     print("End Task ...")
+    return "celery flask test ok!"
+
+
+if __name__ == '__main__':
+    app.run()
 
 # celery -A task worker -l info
+# python3 celery_test.py
+# postman 多窗口 发请求 localhost:5000/celery_flask_test
--- timedTask/celery_basic/readme.md
+++ timedTask/celery_basic/readme.md
@@ -7,3 +7,10 @@ celery -A task worker -l info -P eventlet(会报错)
   
 - 运行celery_test(app).py
 - （可选）运行check验证这个id的值
+
+
+
+- celery+flask 异步请求测试
+    - celery -A task worker -l info
+    - python3 celery_test.py
+    - postman 多窗口 发请求 localhost:5000/celery_flask_test
