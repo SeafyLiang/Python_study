b13e602419579f63de58d758994d29e7d4912334
SeafyLiang <seafyliang@icloud.com>
2022-08-01 14:07:41 +0800

    pandarallel，一行代码并行加速

--- pandas_study/pandas_groupby_parallel.py
+++ pandas_study/pandas_groupby_parallel.py
@@ -9,7 +9,7 @@
 
 from joblib import Parallel, delayed
 import pandas as pd
-
+from pandarallel import pandarallel
 from functools import wraps
 import time
 
@@ -33,8 +33,15 @@ def func_timer(function):
 
 
 def processParallel(df, name):
+    time.sleep(1)
     # 处理数据,如果不加name，return的data没有group信息
-    df['avg_col'] = df['col'].mean()
+    df['avg_b'] = df['b'].mean()
+    return df
+
+
+def mul_process(df):
+    time.sleep(1)
+    df['avg_b'] = df['b'].mean()
     return df
 
 
@@ -45,24 +52,40 @@ def applyParallel(dfGrouped, func):
 
 @func_timer
 def para(df):
-    multi_res = applyParallel(df.groupby('data_index'), processParallel)
+    multi_res = applyParallel(df.groupby('c'), processParallel)
     print(multi_res.info())
     print(multi_res.head())
 
 
 @func_timer
 def single(df):
-    sn_df = df.groupby("data_index")
+    sn_df = df.groupby("c")
     r_df = pd.DataFrame()
     for index, data in sn_df:
-        data['avg_col'] = data['col'].mean()
+        time.sleep(1)
+        data['avg_b'] = data['b'].mean()
         r_df = pd.concat([r_df, data])
     print(r_df.info())
     print(r_df.head())
 
 
+@func_timer
+def multi_para(df):
+    '''
+    推荐使用这个并行加速
+    '''
+    pandarallel.initialize()
+    multi_res = df.groupby("c").parallel_apply(mul_process)
+    print(multi_res.info())
+    print(multi_res.head())
+
+
 if __name__ == '__main__':
-    # 测试数据 440030数据量 7.11s -> 2.27s
-    df = pd.read_csv('test.csv')
+    # 测试数据
+    # [Function: single finished, spent time: 200.72s]
+    # [Function: para finished, spent time: 35.21s]
+    # [Function: multi_para finished, spent time: 25.31s]
+    df = pd.read_csv('data/od_test.csv')
     para(df)
     single(df)
+    multi_para(df)
